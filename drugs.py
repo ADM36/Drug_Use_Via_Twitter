e6be22c00d4b80ee3c29410c607820e2b77bdb1c
Aaron <aarondmiller36@gmail.com>
2019-04-04 11:31:32 -0400

    updating drugs.py to add a dictionary of drug names/values from drug_names_cleaned.csv

--- drugs.py
+++ drugs.py
@@ -1,2 +1,25 @@
+import pandas as pd
+
 #file to hold list or dictionary of drugs
-DRUGS = []
+DRUGS_LIST = []
+DRUGS_DIC = {}
+
+
+drug_file = pd.read_csv("drug_names_cleaned.csv")
+
+print(drug_file.head())
+
+#for drug in drug_file['PROPRIETARYNAME']:
+#    if drug not in DRUGS_DIC:
+#        DRUGS_DIC[drug] = []
+#    else:
+#        pass
+
+# iterate over rows with iterrows()
+for index, row in drug_file.iterrows():
+    if row['PROPRIETARYNAME'] not in DRUGS_DIC:
+        DRUGS_DIC[row['PROPRIETARYNAME']] = [row['NONPROPRIETARYNAME']]
+    else:
+        pass
+
+print(DRUGS_DIC)
